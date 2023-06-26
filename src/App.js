197299393c3ca2a49e4d2a8453ba68dc18497749
hyemdev <devdev.hyem@gmail.com>
2023-06-26 15:49:04 +0900

    feat : 기초 route 연결

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
-import './App.css';
+import Header from "./components/Header";
+
+import Main from "./pages/Main";
+import MonthlyCalendar from "./pages/MonthlyCalendar";
+import Summary from "./pages/Summary";
+import NotFound from "./pages/NotFound";
+
+import "./App.css";
+import { Navigate, Route, Routes } from "react-router";
 
 function App() {
   return (
-    <div className="App">
-..........
+
+    <div>
+      <Header /> 
+      <div>
+        <Routes>
+          <Route path="/" element={<Navigate to="/main" />}></Route>
+          <Route path="/main" element={<Main />}></Route>
+          <Route path="/monthlycalendar" element={<MonthlyCalendar />}></Route>
+          <Route path="/summary" element={<Summary />}></Route>
+          <Route path="*" element={<NotFound />}></Route>
+        </Routes>
+      </div>
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <div>
+      <Link to="/"> main </Link>
+      <Link to="/monthlycalendar"> calendar </Link>
+      <Link to="/summary"> summary </Link>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
